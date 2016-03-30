4e40b0aa7952853d94790327434a9d6a3ac4048a
Reece Trolley <reecetrolley@gmail.com>
2016-03-30 20:32:11 +0100

    added main files

---
+++ client.py
@@ -0,0 +1,293 @@
+import socket
+import tkinter as tk
+from tkinter import messagebox
+
+
+class Login:
+    def __init__(self, main):
+        self.main = main
+
+        self.user_lab = tk.Label(text="Username: ")
+        self.pass_lab = tk.Label(text="Password: ")
+        self.ip_lab = tk.Label(text="Server IP: ")
+
+        self.user_entry = tk.Entry()
+        self.pass_entry = tk.Entry(show="\u2022")
+        self.ip_entry = tk.Entry()
+
+        self.signup_but = tk.Button(text="Sign up", command=self.signup)
+        self.login_but = tk.Button(text="Login", command=self.login)
+
+    def draw(self):
+        self.user_lab.grid(column=0, row=0)
+        self.pass_lab.grid(column=0, row=1)
+        self.ip_lab.grid(column=0, row=2)
+
+        self.user_entry.grid(column=1, row=0, columnspan=3)
+        self.pass_entry.grid(column=1, row=1, columnspan=3)
+        self.ip_entry.grid(column=1, row=2, columnspan=3)
+
+        self.signup_but.grid(column=0, row=3, columnspan=3)
+        self.login_but.grid(column=2, row=3)
+
+    def undraw(self):
+        self.user_lab.grid_forget()
+        self.pass_lab.grid_forget()
+        self.ip_lab.grid_forget()
+
+        self.user_entry.grid_forget()
+        self.pass_entry.grid_forget()
+        self.ip_entry.grid_forget()
+
+        self.signup_but.grid_forget()
+        self.login_but.grid_forget()
+
+    def connect(self):
+        host = self.ip_entry.get()
+        try:
+            if host:
+                self.main.s.connect((host, 54321))
+            else:
+                self.main.s.connect(self.main.server)
+            return True
+
+        except ConnectionRefusedError:
+            self.main.popup("warning", "Could not connect to supplied server.\nIs the server running?")
+            return False
+
+        except OSError as e:
+            if str(e) == "[WinError 10051] A socket operation was attempted to an unreachable network":
+                self.main.popup("warning", "Could not connect to supplied server.\nIs the server running?")
+                return False
+            return True
+
+    # Signup
+    def signup(self):
+        user = self.user_entry.get()
+        passw = self.pass_entry.get()
+
+        if not user or not passw:
+            self.main.popup("warning", "Username or password fields blank.")
+            return
+
+        if self.connect():
+            try:
+                self.main.s.send("#signup:{}:{}".format(user, passw).encode('utf-8'))
+                reply = self.main.s.recv(1024)
+                reply = reply.decode('utf-8')
+                print(reply)
+                if not reply:
+                    self.main.popup("warning", "Server did not respond!")
+                if reply == "#signedup":
+                    self.main.popup("info", "Registered successfully, please log in.")
+                split = reply.split(':')
+                if split[0] == "#error":
+                    if split[1] == "conflict":
+                        self.main.popup("warning", "That username is already in use.")
+            except Exception as e:
+                print(e)
+
+    # Login
+    def login(self):
+        user = self.user_entry.get()
+        passw = self.pass_entry.get()
+
+        if not user or not passw:
+            self.main.popup("warning", "Username or password fields blank.")
+            return
+
+        if self.connect():
+            try:
+                self.main.s.send("#login:{}:{}".format(user, passw).encode('utf-8'))
+                reply = self.main.s.recv(1024)
+                reply = reply.decode('utf-8')
+                print(reply)
+                if not reply:
+                    self.main.popup("warning", "Server did not respond!")
+
+                if reply == "#loggedin":
+                    self.undraw()
+                    self.main.user = user
+                    self.main.current_window = self.main.game
+                    self.main.current_window.draw()
+                    self.main.current_window.update()
+
+                split = reply.split(':')
+                if split[0] == "#error":
+                    if split[1] == "badlogin":
+                        self.main.popup("warning", "That account doesn't exist.")
+
+            except Exception as e:
+                print(e)
+
+
+class Letters:
+    def __init__(self, main):
+        self.main = main
+
+        self.alph = list("abcdefghijklmnopqrstuvwxyz".upper())
+        self.guessed = []
+        self.buttons = dict()
+        for i in range(0, 26):
+            self.buttons[i] = tk.Button(state="disabled", text=self.alph[i], width=2, command=lambda x=i: self.click(x))
+
+    def draw(self):
+        for but in self.buttons:
+            self.buttons[but].grid(row=3, column=but)
+
+    def undraw(self):
+        for but in self.buttons:
+            self.buttons[but].grid_forget()
+
+    def activate(self):
+        for but in self.buttons:
+            self.buttons[but].configure(state="active")
+
+    def disable(self):
+        for but in self.buttons:
+            self.buttons[but].configure(state="disabled")
+
+    def click(self, id):
+        self.buttons[id].configure(state="disabled")
+        self.guessed.append(self.alph[id].lower())
+        if self.alph[id].lower() in self.main.game.word:
+            disp = " ".join([char if char in self.guessed else "_" for char in self.main.game.word])
+            self.main.game.word_lab.configure(text=disp.upper())
+            if "".join(self.main.game.word) == disp.replace(" ", ""):
+                self.main.popup("info", "Correct!")
+                self.disable()
+                try:
+                    self.main.s.send("#finished:{}".format(self.main.user).encode('utf-8'))
+                    reply = self.main.s.recv(1024)
+                    reply = reply.decode('utf-8')
+                    print(reply)
+                    if not reply:
+                        self.main.popup("warning", "Server did not respond!")
+                    split = reply.split(':')
+                    if split[0] == "#winner":
+                        self.main.popup("info", "{} won that round!".format(split[1]))
+                except Exception as e:
+                    print(e)
+        else:
+            self.main.game.tries -= 1
+            if self.main.game.tries < 0:
+                self.main.popup("info", "You've lost!")
+                self.main.game.lost = True
+                self.disable()
+        self.main.game.hangman.configure(image=self.main.game.photos[9-self.main.game.tries])
+        self.main.game.hangman.image = self.main.game.photos[9-self.main.game.tries]
+
+
+class Game:
+    def __init__(self, main):
+        self.main = main
+        self.tries = 9
+        self.lost = False
+        self.word = ""
+
+        # Pictures
+        self.photos = dict()
+        for i in range(0, 10):
+            self.photos[i] = tk.PhotoImage(file="{}.gif".format(i))
+
+        self.hangman = tk.Label(image=self.photos[0])
+        self.hangman.image = self.photos[0]
+
+        self.word_lab = tk.Label(text="Waiting for word...")
+        self.letters = Letters(main)
+
+        self.counter_lab = tk.Label(text=45)
+
+    def draw(self):
+        self.counter_lab.grid(column=0, row=0, columnspan=26)
+        self.hangman.grid(column=0, row=1, columnspan=26)
+        self.word_lab.grid(column=0, row=2, columnspan=26)
+        self.letters.draw()
+
+    def undraw(self):
+        self.counter_lab.grid_forget()
+        self.hangman.grid_forget()
+        self.word_lab.grid_forget()
+        self.letters.undraw()
+
+    def update(self):
+        try:
+            self.main.s.send("#gettime".encode('utf-8'))
+            reply = self.main.s.recv(1024)
+            counter = reply.decode('utf-8')
+            print(counter)
+            if not counter:
+                self.main.popup("warning", "Server did not respond!")
+            if counter.isdigit():
+                self.counter_lab.configure(text=str(int(counter)-1))
+                if self.lost and int(counter) == 1:
+                    try:
+                        self.main.s.send("#lost".encode('utf-8'))
+                        reply = self.main.s.recv(1024)
+                        reply = reply.decode('utf-8')
+                        print(reply)
+                        if not reply:
+                            self.main.popup("warning", "Server did not respond!")
+                        split = reply.split(':')
+                        if split[0] == "#winner":
+                            self.main.popup("info", "{} won that round!".format(split[1]))
+                    except Exception as e:
+                        print(e)
+
+                if int(counter) == 45:
+                    print("getting word")
+                    self.main.s.send("#getword".encode('utf-8'))
+                    reply = self.main.s.recv(1024)
+                    self.word = reply.decode('utf-8')
+                    if not self.word:
+                        self.main.popup("warning", "Server did not respond!")
+                    else:
+                        self.word_lab.configure(text="_ " * len(self.word))
+                        self.letters.activate()
+                        self.letters.guessed = []
+                elif int(counter) > 45:
+                    self.word_lab.configure(text="Waiting for word...")
+                    self.hangman.configure(image=self.photos[0])
+                    self.hangman.image = self.photos[0]
+                    self.letters.disable()
+                    self.tries = 9
+        except Exception as e:
+            print(e)
+
+        finally:
+            self.main.root.after(1000, self.update)
+
+
+class App:
+    def __init__(self):
+        # App
+        self.root = tk.Tk()
+        self.root.title("C'mon boys")
+
+        # Networking
+        self.s = socket.socket()
+        self.s.settimeout(2)
+        self.server = (socket.gethostname(), 54321)
+        self.current_window = ''
+
+        # UI declare
+        self.login = Login(self)
+        self.game = Game(self)
+
+        # Build app
+        self.current_window = self.login
+        self.current_window.draw()
+
+        self.root.mainloop()
+
+    @staticmethod
+    def popup(box, msg):
+        if box == "info":
+            messagebox.showinfo("Information", msg)
+        if box == "warning":
+            messagebox.showwarning("Warning", msg)
+        if box == "error":
+            messagebox.showerror("Error", msg)
+
+
+app = App()
---
+++ sever.py
@@ -0,0 +1,123 @@
+import socket
+import threading
+import random
+import time
+host = socket.gethostname()
+port = 54321
+word = ""
+counter = 60
+initial = 20
+cooldown = 5
+winner = ''
+
+
+# Pick new word every 45 seconds
+def Game():
+    words = ["reece", "python", "code", "jazz", "rhythm", "counterstrike", "ainsley", "harriot",  # LOTS OF WORDS -->
+             'handy', 'planes', 'mist', 'route', 'impossible', 'summer', 'dogs', 'dark', 'young', 'productive', 'play', 'rhythm', 'descriptive', 'straw', 'nifty', 'use', 'reduce', 'fretful', 'ray', 'coil', 'increase', 'psychotic', 'trade', 'slow', 'place', 'memorise', 'middle', 'calculating', 'pale', 'hot', 'bent', 'authority', 'yard', 'whisper', 'nonchalant', 'discover', 'contain', 'noxious', 'adhesive', 'spill', 'notebook', 'substantial', 'sheep', 'appear', 'selection', 'difficult', 'meek', 'profit', 'white', 'diligent', 'carriage', 'rightful', 'trite', 'poised', 'book', 'sneeze', 'post', 'form', 'chemical', 'shirt', 'cautious', 'abrasive', 'learn', 'stone', 'prefer', 'mother', 'nippy', 'umbrella', 'crown', 'tested', 'pump', 'handsome', 'profuse', 'tug', 'frogs', 'reaction', 'drab', 'lake', 'box', 'suppose', 'enchanted', 'hideous', 'conscious', 'rough', 'pets', 'miniature', 'gifted', 'grin', 'abnormal', 'undesirable', 'standing', 'smart', 'deranged', 'number', 'green', 'bumpy', 'top', 'tacky', 'breakable', 'left', 'annoyed', 'tramp', 'celery', 'annoy', 'snotty', 'cry', 'rod', 'meaty', 'swim', 'coach', 'bomb', 'obeisant', 'greet', 'lush', 'stew', 'bridge', 'clap', 'walk', 'sniff', 'replace', 'night', 'ice', 'dramatic', 'eatable', 'correct', 'blush', 'juice', 'lock', 'deadpan', 'witty', 'month', 'scattered', 'rabbits', 'hard', 'messy', 'matter', 'man', 'somber', 'thank', 'concentrate', 'righteous', 'tail', 'cheer', 'muscle', 'gentle', 'damaging', 'savory', 'yam', 'grumpy', 'curvy', 'wretched', 'command', 'bright', 'amusing', 'instruct', 'last', 'excuse', 'wink', 'tan', 'ablaze', 'mom', 'sack', 'chief', 'borrow', 'baby', 'year', 'pipe', 'bushes', 'squealing', 'tiger', 'barbarous', 'record', 'self', 'change', 'wound', 'doctor', 'death', 'aspiring', 'berserk', 'business', 'puzzled', 'complex', 'yummy', 'ultra', 'wrist', 'form', 'admit', 'zoom', 'small', 'stiff', 'uppity', 'cemetery', 'cobweb', 'cheerful', 'guttural', 'expansion', 'quarter', 'selective', 'macabre', 'unlock', 'coil', 'library', 'deer', 'crack', 'trap', 'jail', 'robin', 'afraid', 'terrify', 'waste', 'useless', 'blow', 'shade', 'fertile', 'glow', 'filthy', 'fluffy', 'film', 'flight', 'tickle', 'modern', 'heat', 'blue-eyed', 'regret', 'smiling', 'name', 'sedate', 'side', 'pretty', 'quickest', 'things', 'drown', 'military', 'wild', 'bless', 'eggs', 'pet', 'dangerous', 'horses', 'smell', 'dear', 'punch', 'airport', 'grip', 'smash', 'suggest', 'window', 'cake', 'addition', 'like', 'rhetorical', 'digestion', 'sincere', 'suspend', 'necessary', 'panoramic', 'stain', 'creator', 'marry', 'gold', 'knee', 'vacuous', 'famous', 'trade', 'abrupt', 'friendly', 'attack', 'lucky', 'scientific', 'fascinated', 'baseball', 'peel', 'medical', 'hurried', 'unfasten', 'smile', 'spark', 'rail', 'fry', 'purring', 'equal', 'dock', 'ambitious', 'dinner', 'distance', 'rhyme', 'straight', 'hose', 'hope', 'spring', 'miscreant', 'match', 'fool', 'previous', 'questionable', 'cactus', 'prickly', 'horse', 'sharp', 'unite', 'level', 'seemly', 'truculent', 'imported', 'overt', 'stingy', 'axiomatic', 'try', 'shaky', 'ground', 'bottle', 'switch', 'question', 'offer', 'cough', 'taste', 'throat', 'dead', 'ill-informed', 'cast', 'board', 'amazing', 'dog', 'drip', 'feeble', 'fierce', 'responsible', 'few', 'adamant', 'smell', 'versed', 'tangy', 'pleasant', 'pot', 'recognise', 'fact', 'dolls', 'useful', 'act', 'mouth', 'highfalutin', 'joke', 'helpful', 'gruesome', 'quick', 'spicy', 'bump', 'enjoy', 'floor', 'immense', 'receipt', 'wiggly', 'sisters', 'scene', 'foot', 'fortunate', 'swanky', 'stir', 'fuzzy', 'sordid', 'flippant', 'damage', 'quiet', 'curious', 'gun', 'current', 'stitch', 'pray', 'sidewalk', 'thundering', 'branch', 'show', 'shop', 'boot', 'pricey', 'tow', 'wry', 'existence', 'desert', 'stick', 'travel', 'precious', 'earth', 'light', 'reflect', 'quixotic', 'threatening', 'prose', 'tearful', 'willing', 'wax', 'permissible', 'soothe', 'reign', 'structure', 'hunt', 'quack', 'bruise', 'own', 'battle', 'slim', 'question', 'boundless', 'delicious', 'omniscient', 'battle', 'chance', 'mountain', 'match', 'redundant', 'lumpy', 'drain', 'eminent', 'steep', 'ski', 'phone', 'doll', 'paste', 'flash', 'wish', 'bitter', 'scare', 'hospital', 'drawer', 'efficacious', 'copy', 'vivacious', 'square', 'late', 'eggnog', 'strange', 'enormous', 'time', 'collar', 'ocean', 'awesome', 'detail', 'loose', 'change', 'soap', 'pinch', 'noiseless', 'stocking', 'base', 'chase', 'marvelous', 'mundane', 'neighborly', 'long-term', 'greasy', 'look', 'remain', 'detect', 'release', 'special', 'church', 'crow', 'invent', 'gate', 'introduce', 'chunky', 'silk', 'yak', 'tongue', 'retire', 'kneel', 'bird', 'meddle', 'frame', 'queen', 'follow', 'steer', 'fade', 'sick', 'physical', 'test', 'incandescent', 'add', 'old-fashioned', 'purpose', 'cook', 'pointless', 'numberless', 'harass', 'boat', 'jewel', 'swing', 'zephyr', 'successful', 'yell', 'notice', 'remarkable', 'sail', 'tire', 'glamorous', 'workable', 'tasteless', 'record', 'representative', 'educated']
+    global word
+    global counter
+    global initial
+    global cooldown
+    global winner
+    while 1:
+        print(initial+cooldown+counter)
+        if initial == 0:
+            if cooldown <= 1:
+                if cooldown:
+                    if counter == 60:
+                        word = random.choice(words)
+                        winner = ''
+                        print(word)
+                        cooldown -= 1
+                    continue
+                time.sleep(1)
+                counter -= 1
+                if counter == 0:
+                    counter = 60
+                    cooldown = 5
+            else:
+                cooldown -= 1
+                time.sleep(1)
+        else:
+            initial -= 1
+            time.sleep(1)
+
+
+def handler(c, addr):
+    global word
+    global counter
+    global initial
+    global cooldown
+    global winner
+    while 1:
+        try:
+            data = c.recv(1024)
+            if not data:
+                break
+            data = data.decode('utf-8')
+            print(str(addr) + " recv: " + data)
+            split = data.split(':')
+            # If data is a tagged message
+            if split[0][0] == '#':
+                # Login
+                if split[0] == "#login" and len(split) == 3:
+                    with open("login.txt", 'r') as f:
+                        lines = [x.strip('\n') for x in f.readlines() if x != '\n']
+                    if "{},{}".format(split[1], split[2]) in lines:
+                        c.send("#loggedin".encode('utf-8'))
+                    else:
+                        c.send("#error:badlogin".encode('utf-8'))
+                # Signup
+                elif split[0] == "#signup" and len(split) == 3:
+                    with open("login.txt", 'r') as f:
+                        lines = [x.strip('\n').split(',') for x in f.readlines() if x != '\n']
+                    if not [x for x in lines if x[0] == split[1]]:
+                        lines.append([split[1], split[2]])
+                        with open("login.txt", 'w') as f:
+                            f.write('\n'.join([','.join(x) for x in lines]))
+                        c.send("#signedup".encode('utf-8'))
+                    else:
+                        c.send("#error:conflict".encode('utf-8'))
+                # Request word
+                elif split[0] == "#getword":
+                    print("sending", word.encode('utf-8'))
+                    c.send(word.encode('utf-8'))
+                elif split[0] == "#gettime":
+                    if initial or cooldown or not word:
+                        c.send(str(60).encode('utf-8'))
+                    else:
+                        c.send(str(counter).encode('utf-8'))
+                elif split[0] == "#finished":
+                    if not winner:
+                        winner = split[1]
+                    c.send("#winner:{}".format(winner).encode('utf-8'))
+                elif split[0] == "#lost":
+                    c.send("#winner:{}".format(winner).encode('utf-8'))
+            if "close" == data:
+                break
+
+        except Exception as e:
+            print(e)
+            break
+
+    c.close()
+    print(addr, "- closed connection")
+
+server = (host, port)
+
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(server)
+
+s.listen(5)
+
+game_t = threading.Thread(target=Game)
+game_t.start()
+
+while 1:
+    print("listening on", host, port)
+    client, address = s.accept()
+    print("... connected from", address)
+    t = threading.Thread(target=handler, args=(client, address))
+    t.start()
